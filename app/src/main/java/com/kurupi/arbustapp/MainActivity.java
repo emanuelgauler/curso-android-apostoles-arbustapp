d7973e5512d63094163ad23863b1c5eca234c9ba
Emanuel Gauler <emanuelgauler@gmail.com>
2019-07-04 22:13:11 -0300

    Clase que gestiona la edición de especie y sus ejemplares

--- app/src/main/java/com/kurupi/arbustapp/MainActivity.java
+++ app/src/main/java/com/kurupi/arbustapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.kurupi.arbustapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.kurupi.arbustapp.ui.ActividadEditarEspecie;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +22,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        RecyclerView vista_de_especies = findViewById(R.id.recurso_vista_de_especies);
+
+        FloatingActionButton fab = findViewById(R.id.recurso_boton_agregar_especie);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                iniciar_actividad_para_editar_una_especie(view);
             }
         });
+    }
+
+    private void iniciar_actividad_para_editar_una_especie(View view) {
+        Intent lanzar_actividad_para_editar_una_especie = new Intent(MainActivity.this, ActividadEditarEspecie.class);
+        startActivity(lanzar_actividad_para_editar_una_especie);
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
     @Override
---
+++ app/src/main/java/com/kurupi/arbustapp/ui/ActividadEditarEspecie.java
@@ -0,0 +1,16 @@
+package com.kurupi.arbustapp.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import com.kurupi.arbustapp.R;
+
+public class ActividadEditarEspecie extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.esquema_para_editar_especie);
+    }
+}
